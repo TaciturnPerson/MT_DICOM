9ba9bf20b743c9c145693ee13b58caf07d30ce89
TaciturnPerson <znviktor@gmail.com>
2020-09-24 17:40:23 +0300

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,5 @@
 /* global module */
-var express = require('express');
-var app     = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
 
 
 module.exports = function(grunt) {
