720aadf0b04f723064ef52641db4c8b3c181ff38
TaciturnPerson <znviktor@gmail.com>
2020-09-27 19:51:39 +0300

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,8 @@ app.set('port', (process.env.PORT || 5000));
 //For avoidong Heroku $PORT error
 app.get('/', function() {
     grunt.registerTask('init', ['connect:init']);
-    var port = app.get('port');
 });
-
+var port = app.get('port');
 
 module.exports = function(grunt) {
     // Project configuration
