0b4b5b6c6b22473291cd5df0a23958fc18266fff
TaciturnPerson <znviktor@gmail.com>
2020-09-27 19:37:39 +0300

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,5 +34,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Task to run tests
-    grunt.registerTask('start', ['connect']);
+    grunt.registerTask('start', ['connect', 'watch']);
 };
