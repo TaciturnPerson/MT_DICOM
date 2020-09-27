026e648c7029d8d20a312b80d6bd112c8ae8e590
TaciturnPerson <znviktor@gmail.com>
2020-09-27 20:22:02 +0300

    Test

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+import * as grunt from "grunt";
+
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var PORT = process.env.PORT || 3000;
-var FACTORIAL = path.join(__dirname, 'build', 'factorial.min.js');
 
 express()
   .use(hello)
   .listen(PORT, onListen);
 
-function hello(req, res, next) {
-  res.sendFile(FACTORIAL);
+function hello() {
+  grunt.registerTask('start', ['connect']);
 }
 
 function onListen() {
