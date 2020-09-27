009a324bdf6b69109fce7cb757af2606940ec7d9
TaciturnPerson <znviktor@gmail.com>
2020-09-27 20:26:08 +0300

    Test

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ var path = require('path');
 var PORT = process.env.PORT || 3000;
 
 express()
-  .use(hello)
   .listen(PORT, onListen);
-
-function hello(res) {
-  res.sendFile('index.html');
-}
 
 function onListen() {
   console.log('Listening on', PORT);
