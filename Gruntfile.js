d5bea06169cb40f320c3f0c8217b3751d30d8755
TaciturnPerson <znviktor@gmail.com>
2020-09-27 17:27:55 +0300

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,21 @@
 /* global module */
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/').listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
 
 module.exports = function(grunt) {
     // Project configuration
+
+    var express = require('express');
+    var app     = express();
+
+    app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+    app.get('/', function(request, response) {
+        var result = grunt.registerTask('start', ['connect:prod', 'watch'])
+        response.send(result);
+    }).listen(app.get('port'), function() {
+        console.log('App is running, server is listening on port ', app.get('port'));
+    });
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
