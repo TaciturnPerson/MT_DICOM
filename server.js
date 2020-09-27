01cae51237bab55d7e75dff4745047449f488e2e
TaciturnPerson <znviktor@gmail.com>
2020-09-27 22:08:17 +0300

    Test

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var PORT = process.env.PORT || 3000;
+var app = express();
 
 express()
     .use(hello)
     .listen(PORT, onListen);
 
+app.use(express.static('MT_DICOM'));
+
 function hello(req, res, next) {
-  res.sendFile(path.join(__dirname, '/', 'index.html'));
+  res.sendFile('index.html');
 }
 
 function onListen() {
