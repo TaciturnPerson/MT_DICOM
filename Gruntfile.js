67e68d7e1ee5af70966b2d5674ae4687137a2b92
TaciturnPerson <znviktor@gmail.com>
2020-09-27 20:15:00 +0300

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         connect: {
             server: {
                 options: {
-		        port: 5000,
+		        port: process.env.PORT || 5000,
 		        hostname: "0.0.0.0",
                     livereload: 35729,
                     keepalive: true,
